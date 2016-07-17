dbca3f3e3124809a9072185e666e5db908ce921c
Carlos <carlos.w.med@gmail.com>
2016-07-17 00:25:08 -0400

    Checking to see if moving mac install works better in the setup.sh

--- installs/setup.sh
+++ installs/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. mac.sh
+# . mac.sh
 
 blink=$(tput blink)
 normal=$(tput sgr0)
@@ -9,7 +9,32 @@ blue=$(tput setaf 4)
 
 clear
 
+# Installation Functions
+mac () {
+	printf $red
+	echo "*******************************"
+	echo "     Preparing Installation    "
+	echo "           For $os             "
+	echo "*******************************"
+	printf $normal
+	sleep 2s
 
+	printf "%s\n" $blue
+	echo "#########################"
+	echo "###  Installing Brew  ###"
+	echo "#########################"
+
+	printf "%s\n" $green
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+	printf "%s\n" $blue
+	echo "#########################"
+	echo "###  Installing Brew  ###"
+	echo "###      Packages     ###"
+	echo "#########################"
+
+	brew install git mongodb neofetch openssl dpkg
+}
 
 read -p "${green}What OS are you on: (ie mac, linux, windows) ${red}" os
 
