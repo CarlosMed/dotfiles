f18ae6db2e46d33e5825c0334e85e53849b9ad4b
Carlos <carlos.w.med@gmail.com>
2016-07-21 01:56:05 -0400

    Testing source direct URL install for setup.sh

--- installs/setup.sh
+++ installs/setup.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-# . mac.sh
+#!/bin/sh
+source https://raw.githubusercontent.com/CarlosMed/dotfiles/master/installs/mac.sh
+source https://raw.githubusercontent.com/CarlosMed/dotfiles/master/installs/arch.sh
 
 blink=$(tput blink)
 normal=$(tput sgr0)
@@ -9,33 +10,6 @@ blue=$(tput setaf 4)
 
 clear
 
-# Installation Functions
-mac () {
-	printf $red
-	echo "*******************************"
-	echo "     Preparing Installation    "
-	echo "           For $os             "
-	echo "*******************************"
-	printf $normal
-	sleep 2s
-
-	printf "%s\n" $blue
-	echo "#########################"
-	echo "###  Installing Brew  ###"
-	echo "#########################"
-
-	printf "%s\n" $green
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-	printf "%s\n" $blue
-	echo "#########################"
-	echo "###  Installing Brew  ###"
-	echo "###      Packages     ###"
-	echo "#########################"
-
-	brew install git mongodb neofetch openssl dpkg
-}
-
 read -p "${green}What OS are you on: (ie mac, linux, windows) ${red}" os
 
 if [[ $os == "mac" ]]; then
@@ -43,7 +17,8 @@ if [[ $os == "mac" ]]; then
 	mac
 
 elif [[ $os == "arch" ]]; then
-	$os
+	clear
+	arch
 
 else
 	echo "Please try again!"
