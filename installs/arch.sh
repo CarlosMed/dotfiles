9cf214d0a2aefc67ce4b36742a94fb63126e3695
Carlos <carlos.w.med@gmail.com>
2016-07-23 18:49:30 -0400

    Fixing direct link for mac.sh

---
+++ installs/arch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+arch () {
+
+echo "hello arch"
+}
--- installs/setup.sh
+++ installs/setup.sh
@@ -1,12 +1,27 @@
-#!/bin/sh
-source mac.sh
-source arch.sh
+#!/bin/bash
+#. ~/dotfiles/installs/mac.sh
+#. ~/dotfiles/installs/arch.sh
+
+source https://raw.githubusercontent.com/CarlosMed/dotfiles/master/installs/mac.sh
 
 blink=$(tput blink)
 normal=$(tput sgr0)
 green=$(tput setaf 150)
 red=$(tput setaf 1)
 blue=$(tput setaf 4)
+
+clear
+
+# git check
+# printf "%s\n" $blue
+# echo "#########################"
+# echo "###    Checking Git   ###"
+# echo "#########################"
+
+# if [[ $(git --version) ]]; then
+# 	echo "Git already installed"
+# 	git clone https://github.com/carlosmed/dotfiles.git
+# fi
 
 clear
 
