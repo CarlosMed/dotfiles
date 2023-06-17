dc1a2da1f0c09bc546e212b6d21e4d803a1b9b38
Carlos Medina <carlos.medina@method.com>
2023-06-17 18:22:12 -0400

    fear: removal of git check into individual installation and normal printing

--- setup.sh
+++ setup.sh
@@ -13,21 +13,7 @@ normal=$(tput sgr0)
 green=$(tput setaf 150)
 red=$(tput setaf 1)
 blue=$(tput setaf 4)
-
-clear
-
-# git check
-printf "%s\n" $blue
-echo "#########################"
-echo "###    Checking Git   ###"
-echo "#########################"
-
-if [[ $(git --version) ]]; then
-	echo "Git already installed"
-	# git clone https://github.com/carlosmed/dotfiles.git
-fi
-
-clear
+nl=$(printf "%s\n" $normal)
 
 read -p "${green}What OS are you on: (ie mac, arch, windows) ${red}" os
 
