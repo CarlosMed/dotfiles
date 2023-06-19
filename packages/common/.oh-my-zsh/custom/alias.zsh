6dd700a966f25332250844f4ef52ef7d410cd7f5
Carlos Medina <carlos.medina@method.com>
2023-06-19 14:23:43 -0400

    feat: added some extra aliases

--- packages/common/.oh-my-zsh/custom/alias.zsh
+++ packages/common/.oh-my-zsh/custom/alias.zsh
@@ -1,18 +1,41 @@
-# Generic
-alias reboot="_ reboot"
-alias shutdown="_ shutdown"
-alias huh="alias | grep"
-alias trash="npx trash-cli"
-alias tup="tmux source-file ~/.tmux.conf"
-alias zup="exec zsh" # View: https://unix.stackexchange.com/questions/532548/how-do-i-apply-the-changes-to-the-zshrc-file-after-editing-it
-# alias zup="source ~/.zshrc"
-
 # Programming
-alias v="vim"
 alias c="code"
 alias nr="npm run"
+alias v="vim"
 
 # Arch shortcuts
-alias stx="startx"
 alias pacman="_ pacman"
+alias stx="startx"
 alias xup="xrdb ~/.Xresources"
+
+# Generic
+
+alias huh="alias | grep"
+alias lip="ipconfig getifaddr en0"
+alias reboot="_ reboot"
+alias shutdown="_ shutdown"
+alias trash="npx trash-cli"
+alias tup="tmux source-file ~/.tmux.conf"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Disable Spotlight
+alias spotoff="sudo mdutil -a -i off"
+# Enable Spotlight
+alias spoton="sudo mdutil -a -i on"
+
+# View: https://unix.stackexchange.com/questions/532548/how-do-i-apply-the-changes-to-the-zshrc-file-after-editing-it
+alias zup="exec zsh"
+# alias zup="source ~/.zshrc"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
