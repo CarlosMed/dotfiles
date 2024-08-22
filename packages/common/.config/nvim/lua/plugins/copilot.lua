ad5eddd0821381b579d7326e3955ead5b38f6237
Carlos Medina <carlos.w.med@gmail.com>
2024-08-22 19:15:20 -0400

    feat: disabled copilot

--- packages/common/.config/nvim/lua/plugins/copilot.lua
+++ packages/common/.config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    -- enabled = false,
+    enabled = false,
     event = "InsertEnter",
     build = ":Copilot auth",
     opts = {
