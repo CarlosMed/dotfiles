c2a869dce1fe987d44413f3e0e6322e8a900d76a
Carlos Medina <carlos.w.med@gmail.com>
2024-09-22 13:13:24 -0400

    fix: nvim option formatting

--- packages/common/.config/nvim/lua/core/options.lua
+++ packages/common/.config/nvim/lua/core/options.lua
@@ -1,30 +1,30 @@
 --[[ Apperance ]]
 vim.opt.termguicolors = true -- turn on termguicolors for colorscheme
-vim.opt.background = "dark" -- setting colorschemes that can be light or dark will be made dark
+vim.opt.background = "dark"  -- setting colorschemes that can be light or dark will be made dark
 
-vim.opt.nu = true -- adds numbers to the gutter
-vim.opt.rnu = true -- adds relative numbers to the gutters
-vim.o.mouse = "a" -- allows the use of the mouse
+vim.opt.nu = true            -- adds numbers to the gutter
+vim.opt.rnu = true           -- adds relative numbers to the gutters
+vim.o.mouse = "a"            -- allows the use of the mouse
 
-vim.opt.colorcolumn = "80" -- adds a column at the 80pt mark
-vim.opt.cursorline = true -- highlights cursor line
-vim.wo.signcolumn = "yes" -- show sign column so that text doesn't shift
+vim.opt.colorcolumn = "80"   -- adds a column at the 80pt mark
+vim.opt.cursorline = true    -- highlights cursor line
+vim.wo.signcolumn = "yes"    -- show sign column so that text doesn't shift
 
-vim.opt.list = true -- show whitespace
+vim.opt.list = true          -- show whitespace
 -- vim.opt.listchars = { space = "⋅", trail = "⋅", tab = "  " } -- whitespace character to show
 vim.opt.listchars = { tab = "⇥ ", space = "⋅", leadmultispace = "┊ ", trail = "␣", nbsp = "⍽" }
 
 -- makes cursor blink
 vim.opt.guicursor =
-  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- Tabs, indents, word wrap
-vim.opt.tabstop = 2 -- 4 spaces for tabs
-vim.opt.shiftwidth = 2 -- 2 spaces for indent width
-vim.opt.expandtab = true -- expand tabs to spaces
-vim.opt.autoindent = true -- copy indent from current line when adding a new line
+vim.opt.tabstop = 2        -- 4 spaces for tabs
+vim.opt.shiftwidth = 2     -- 2 spaces for indent width
+vim.opt.expandtab = true   -- expand tabs to spaces
+vim.opt.autoindent = true  -- copy indent from current line when adding a new line
 vim.opt.smartindent = true -- indents lines smartly
-vim.opt.wrap = true -- option to wrap text
+vim.opt.wrap = true        -- option to wrap text
 vim.opt.softtabstop = 2
 
 --[[ Behavior ]]
@@ -39,7 +39,7 @@ vim.opt.isfname:append("@-@")
 
 --code folding
 vim.o.foldcolumn = "0" -- '0' is not bad
-vim.o.foldlevel = 99 -- using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99   -- using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.opt.fillchars = {
@@ -54,8 +54,8 @@ vim.opt.fillchars = {
 -- vim.o.foldclose = "all"
 
 -- searching
-vim.o.ignorecase = true -- case-insensitive searching UNLESS \C or capital in search
-vim.o.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+vim.o.ignorecase = true  -- case-insensitive searching UNLESS \C or capital in search
+vim.o.smartcase = true   -- if you include mixed case in your search, assumes you want case-sensitive
 vim.opt.hlsearch = false -- turns off highlight search
 vim.opt.incsearch = true -- turns on incremental search
 
