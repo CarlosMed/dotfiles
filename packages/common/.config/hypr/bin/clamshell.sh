e21e0fdb90120cd7b0e04a30a84fc143e258f13b
Carlos Medina <carlos.w.med@gmail.com>
2024-10-09 12:28:47 -0400

    fix: disable LG monitor breaking eDP

--- packages/common/.config/hypr/bin/clamshell.sh
+++ packages/common/.config/hypr/bin/clamshell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 if grep open /proc/acpi/button/lid/LID/state; then
-  hyprctl keyword monitor "eDP-1,highrr,auto,1"
-  hyprctl keyword monitor "desc:ViewSonic Corporation XG270QG,2560x1440@59.95,auto,1"
-  hyprctl keyword monitor "desc:LG Electronics LG HDR 4K 0x00017916,1920x1080@59.94,auto,1,transform,3"
+  hyprctl keyword monitor "BOE 0x0791,1920x1080@60.01,auto,1"
+  hyprctl keyword monitor "DP-1,disable"
+  hyprctl keyword monitor "desc:LG Electronics LG HDR 4K 0x00017916,disable"
 else
   if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
     hyprctl keyword monitor "eDP-1,disable"
