7b29bc0fb43541126962a1ac418dd3c8e3873936
Carlos Medina <carlos.w.med@gmail.com>
2024-10-23 22:36:24 -0400

    fix: powermenu selection and styling

--- packages/common/.config/hypr/bin/powermenu.sh
+++ packages/common/.config/hypr/bin/powermenu.sh
@@ -2,7 +2,6 @@
 
 rofi \
   -show p \
-  -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
-  -font "JetBrains Mono NF 16" \
-  -theme ~/.config/rofi/config.rasi \
-  -theme-str 'listview {lines: 6;}'
+  -modi p:'rofi-power-menu --choices=suspend/reboot/shutdown' \
+  -font 'Hack Nerd Font Bold 16' \
+  -theme-str 'listview {lines: 3; columns: 1;} window {height: 20em; width: 20em;} mainbox { orientation: vertical;} mode-switcher { enabled: false;}'
