0c10543b36de32b0d1e0736d097f452fae99ba8b
Carlos Medina <carlos.w.med@gmail.com>
2024-12-25 18:41:09 -0500

    feat: moving diffview to its own config

---
+++ packages/common/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Diff View Open" },
+    { "<leader>gdq", "<cmd>DiffviewClose<cr>", desc = "Diff View Open" },
+  },
+}
