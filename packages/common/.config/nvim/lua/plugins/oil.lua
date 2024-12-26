e5a7975256864f16eef7b6d223ca076ff33a1f7f
Carlos Medina <carlos.w.med@gmail.com>
2024-12-25 19:06:09 -0500

    feat: nvim oil keymap remap

--- packages/common/.config/nvim/lua/plugins/oil.lua
+++ packages/common/.config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,36 @@
 return {
   "stevearc/oil.nvim",
   opts = {
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-h>"] = "actions.select_split",
+      ["<C-v>"] = "actions.select_vsplit",
+      ["<C-t>"] = "actions.select_tab",
+      ["<C-p>"] = "actions.preview",
+      ["q"] = "actions.close",
+      ["<C-c>"] = "actions.close",
+      ["<C-r>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["gs"] = "actions.change_sort",
+      ["gx"] = "actions.open_external",
+      ["g."] = "actions.toggle_hidden",
+      ["g\\"] = "actions.toggle_trash",
+    },
+    use_default_keymaps = false,
     delete_to_trash = true,
     skip_confirm_for_simple_edits = false,
 
     float = {
       max_height = 20,
       max_width = 60,
+    },
+
+    win_options = {
+      cursorline = true,
     },
 
     view_options = {
