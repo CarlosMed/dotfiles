d218d2f9ab8510bf2ead3446d211a2cbe1bf7edb
Carlos Medina <carlos.w.med@gmail.com>
2025-06-21 00:59:53 -0400

    feat: hardtime

--- packages/common/.config/nvim/lua/plugins/disabled.lua
+++ packages/common/.config/nvim/lua/plugins/disabled.lua
@@ -3,7 +3,7 @@ return {
   { "tpope/vim-fugitive", enabled = false }, -- Git related plugin
   { "tpope/vim-rhubarb", enabled = false }, -- Git related plugin
   { "tpope/vim-sleuth", enabled = false }, -- Detect tabstop and shiftwidth automatically
-  { "m4xshen/hardtime.nvim", enabled = false, opts = {} },
+  { "m4xshen/hardtime.nvim", enabled = true, opts = {} },
   { "NeogitOrg/neogit", enabled = false, config = true, keys = { { "<C-g>", "<cmd>Neogit<CR>", "n" } } },
   {
     "nvim-neo-tree/neo-tree.nvim",
