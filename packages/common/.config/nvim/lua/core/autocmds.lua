28b74436aac124c05451a668ee84cf59680b6b6c
Carlos Medina <carlos.w.med@gmail.com>
2025-08-01 10:33:01 -0400

    feat: new neovim autocommands

--- packages/common/.config/nvim/lua/core/autocmds.lua
+++ packages/common/.config/nvim/lua/core/autocmds.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_create_autocmd({ "FileType" }, {
+local api = vim.api
+
+api.nvim_create_autocmd({ "FileType" }, {
   pattern = {
     "netrw",
     "Jaq",
@@ -22,18 +24,30 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Prevents nvim from opening in insert mode
---[[ vim.api.nvim_create_autocmd("WinLeave", {
-  callback = function()
-    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
-    end
-  end,
-}) ]]
-
-vim.api.nvim_create_autocmd("BufWrite", {
-  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
-  callback = function()
-    vim.cmd("TailwindSort")
-  end,
+-- go to last loc when opening a buffer
+-- this mean that when you open a file, you will be at the last position
+api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
 })
+
+-- resize neovim split when terminal is resized
+vim.api.nvim_command("autocmd VimResized * wincmd =")
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+        pcall(vim.treesitter.start)
+    end,
+})
+
+-- vim.api.nvim_create_autocmd("BufWrite", {
+--   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+--   callback = function()
+--     vim.cmd("TailwindSort")
+--   end,
+-- })
