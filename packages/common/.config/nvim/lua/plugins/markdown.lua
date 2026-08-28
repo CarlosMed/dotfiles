59e32615b46abb474bd7810fbd4cda92a84cece2
Carlos Medina <carlos.w.med@gmail.com>
2026-08-28 11:32:36 -0400

    feat: removal of markdown-preview

--- packages/common/.config/nvim/lua/plugins/markdown.lua
+++ packages/common/.config/nvim/lua/plugins/markdown.lua
@@ -52,14 +52,6 @@ return {
     },
   },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {},
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
