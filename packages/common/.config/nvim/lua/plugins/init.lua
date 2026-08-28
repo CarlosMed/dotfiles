c498662c5188665dd994a4cabfb0b484b1b07f85
Carlos Medina <carlos.w.med@gmail.com>
2026-08-28 11:34:03 -0400

    feat: adding hardline an precognition for nvim

--- packages/common/.config/nvim/lua/plugins/init.lua
+++ packages/common/.config/nvim/lua/plugins/init.lua
@@ -1,9 +1,13 @@
 return {
-  { "nvim-tree/nvim-web-devicons",                 lazy = true },
-  { "MunifTanjim/nui.nvim",                        lazy = true },
-  { "christoomey/vim-tmux-navigator" },                                                                       -- used to navigate vim and tmux
-  { "nvim-lua/plenary.nvim",                       event = { "BufReadPost", "BufWritePost", "BufNewFile" } }, -- needed for many other dependencies
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+  { "MunifTanjim/nui.nvim", lazy = true },
+  { "christoomey/vim-tmux-navigator" }, -- used to navigate vim and tmux
+  { "nvim-lua/plenary.nvim", event = { "BufReadPost", "BufWritePost", "BufNewFile" } }, -- needed for many other dependencies
   -- { "test", dev = true, enabled = false },
-  { "mg979/vim-visual-multi",                      event = { "BufReadPost", "BufWritePost", "BufNewFile" } },
+  { "mg979/vim-visual-multi", event = { "BufReadPost", "BufWritePost", "BufNewFile" } },
   { "joosepalviste/nvim-ts-context-commentstring", lazy = true },
+
+  -- Training
+  { "m4xshen/hardtime.nvim", lazy = false, dependencies = { "MunifTanjim/nui.nvim" }, opts = {} },
+  { "tris203/precognition.nvim", opts = {} },
 }
