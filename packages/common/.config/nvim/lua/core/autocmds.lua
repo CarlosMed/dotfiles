ee4315579c6a11060db557abbcc6f245023c90db
Carlos Medina <carlos.w.med@gmail.com>
2026-08-28 11:30:33 -0400

    feat: adding a few autocmds to nvim

--- packages/common/.config/nvim/lua/core/autocmds.lua
+++ packages/common/.config/nvim/lua/core/autocmds.lua
@@ -24,26 +24,51 @@ api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd("BufRead", {
+  group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+  pattern = { ".env", ".env.*" },
+  callback = function()
+    vim.bo.filetype = "dosini"
+  end,
+})
+
 -- go to last loc when opening a buffer
 -- this mean that when you open a file, you will be at the last position
 api.nvim_create_autocmd("BufReadPost", {
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
 })
 
 -- resize neovim split when terminal is resized
 vim.api.nvim_command("autocmd VimResized * wincmd =")
 
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function()
-        pcall(vim.treesitter.start)
-    end,
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
 })
+
+-- -- no auto continue comments on new line
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+--   callback = function()
+--     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+--   end,
+-- })
 
 -- vim.api.nvim_create_autocmd("BufWrite", {
 --   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
