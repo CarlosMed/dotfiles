4e8ff9569c1afaff2277540d1f9cd3986afb62b9
Carlos Medina <carlos.w.med@gmail.com>
2026-08-29 08:07:42 -0400

    feat: moving more towards native treesitter with modern config

--- packages/common/.config/nvim/lua/plugins/treesitter.lua
+++ packages/common/.config/nvim/lua/plugins/treesitter.lua
@@ -4,52 +4,17 @@ return {
   -- syntax highlighting.
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "main", -- enabled once treesitter works for nvim v12.4
-    version = false, -- last release is way too old and doesn't work on Windows
+    branch = "main",
+    version = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    -- event = { "BufReadPre", "BufNewFile" },
-    -- init = function()
-    --   require("nvim-treesitter.query_predicates")
-    -- end,
-    dependencies = {
-      -- {
-      --   "nvim-treesitter/nvim-treesitter-textobjects",
-      --   config = function()
-      --     -- When in diff mode, we want to use the default
-      --     -- vim text objects c & C instead of the treesitter ones.
-      --     local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-      --     local configs = require("nvim-treesitter.configs")
-      --     for name, fn in pairs(move) do
-      --       if name:find("goto") == 1 then
-      --         move[name] = function(q, ...)
-      --           if vim.wo.diff then
-      --             local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-      --             for key, query in pairs(config or {}) do
-      --               if q == query and key:find("[%]%[][cC]") then
-      --                 vim.cmd("normal! " .. key)
-      --                 return
-      --               end
-      --             end
-      --           end
-      --           return fn(q, ...)
-      --         end
-      --       end
-      --     end
-      --   end,
-      -- },
-    },
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
-    },
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
+    -- Highlighting and indent are handled natively by Neovim via
+    -- `vim.treesitter.start()` (see lua/core/autocmds.lua). Building
+    -- parsers from source on the `main` branch requires `tree-sitter-cli`.
+    -- `install()` is idempotent: it is a no-op for already-installed parsers.
+    config = function()
+      require("nvim-treesitter").install({
         "angular",
         "awk",
         "bash",
@@ -77,7 +42,6 @@ return {
         "jq",
         "jsdoc",
         "json",
-        "jsonc",
         "lua",
         "luadoc",
         "luap",
@@ -102,65 +66,96 @@ return {
         "vue",
         "xml",
         "yaml",
+      }):wait(300000)
+    end,
+    -- Incremental selection: nvim-treesitter `main` removed the old
+    -- incremental_selection module; Neovim provides the same functionality
+    -- natively via `vim.treesitter.select()`.
+    keys = {
+      {
+        "<c-space>",
+        function()
+          vim.treesitter.select("parent")
+        end,
+        desc = "Start treesitter selection",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
+      {
+        "<c-space>",
+        function()
+          vim.treesitter.select("parent")
+        end,
+        desc = "Expand selection",
+        mode = { "x", "o" },
       },
-      -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-      context_commentstring = {
-        enable_autocmd = false,
-        languages = "// %s",
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-        move = {
-          enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        },
+      {
+        "<bs>",
+        function()
+          vim.treesitter.select("child")
+        end,
+        desc = "Shrink selection",
+        mode = { "x", "o" },
       },
     },
-    ---@param opts TSConfig
-    -- config = function(_, opts)
-    --   if type(opts.ensure_installed) == "table" then
-    --     ---@type table<string, boolean>
-    --     local added = {}
-    --     opts.ensure_installed = vim.tbl_filter(function(lang)
-    --       if added[lang] then
-    --         return false
-    --       end
-    --       added[lang] = true
-    --       return true
-    --     end, opts.ensure_installed)
-    --   end
-    --   require("nvim-treesitter.configs").setup(opts)
-    -- end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    deps = { "nvim-treesitter/nvim-treesitter" },
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    config = function()
+      local ts_select = require("nvim-treesitter-textobjects.select")
+      local ts_move = require("nvim-treesitter-textobjects.move")
+
+      vim.keymap.set({ "x", "o" }, "af", function()
+        ts_select.select_textobject("@function.outer", "textobjects")
+      end, { desc = "a function" })
+      vim.keymap.set({ "x", "o" }, "if", function()
+        ts_select.select_textobject("@function.inner", "textobjects")
+      end, { desc = "inner function" })
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        ts_select.select_textobject("@class.outer", "textobjects")
+      end, { desc = "a class" })
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        ts_select.select_textobject("@class.inner", "textobjects")
+      end, { desc = "inner class" })
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        ts_select.select_textobject("@parameter.outer", "textobjects")
+      end, { desc = "a parameter" })
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        ts_select.select_textobject("@parameter.inner", "textobjects")
+      end, { desc = "inner parameter" })
+
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        ts_move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next function start" })
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        ts_move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Previous function start" })
+      vim.keymap.set({ "n", "x", "o" }, "]F", function()
+        ts_move.goto_next_end("@function.outer", "textobjects")
+      end, { desc = "Next function end" })
+      vim.keymap.set({ "n", "x", "o" }, "[F", function()
+        ts_move.goto_previous_end("@function.outer", "textobjects")
+      end, { desc = "Previous function end" })
+      vim.keymap.set({ "n", "x", "o" }, "]c", function()
+        ts_move.goto_next_start("@class.outer", "textobjects")
+      end, { desc = "Next class start" })
+      vim.keymap.set({ "n", "x", "o" }, "[c", function()
+        ts_move.goto_previous_start("@class.outer", "textobjects")
+      end, { desc = "Previous class start" })
+      vim.keymap.set({ "n", "x", "o" }, "]C", function()
+        ts_move.goto_next_end("@class.outer", "textobjects")
+      end, { desc = "Next class end" })
+      vim.keymap.set({ "n", "x", "o" }, "[C", function()
+        ts_move.goto_previous_end("@class.outer", "textobjects")
+      end, { desc = "Previous class end" })
+    end,
   },
 
   -- Automatically add closing tags for HTML and JSX
   {
     "windwp/nvim-ts-autotag",
-    -- event = "LazyFile",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     opts = {
       opts = {
